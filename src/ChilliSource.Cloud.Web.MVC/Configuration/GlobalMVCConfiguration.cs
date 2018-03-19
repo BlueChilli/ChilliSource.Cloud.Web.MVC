6e50b10d0b9e51a927682adaa87c7ff3fa2af360
david <drgrieve@hotmail.com>
2018-03-19 16:54:58 +1100

    prune out some dead code

--- src/ChilliSource.Cloud.Web.MVC/Configuration/GlobalMVCConfiguration.cs
+++ src/ChilliSource.Cloud.Web.MVC/Configuration/GlobalMVCConfiguration.cs
@@ -14,32 +14,5 @@ namespace ChilliSource.Cloud.Web.MVC
         public static GlobalMVCConfiguration Instance { get { return _instance; } }
 
         private GlobalMVCConfiguration() { }
-
-        public string StylesDirectory { get; set; } = "~/Styles/";
-        public string ImagesDirectory { get; set; } = "~/Images/";
-        public string ScriptsDirectory { get; set; } = "~/Scripts/";
-        public string DefaultFieldCSS { get; internal set; }
-
-        internal string GetPath(DirectoryType type, string filename)
-        {
-            return GetDirectory(type) + filename;
-        }
-
-        /// <summary>
-        /// Get directory path from configuration file based on the BlueChilli.Lib.Configuration.DirectoryType.
-        /// </summary>
-        /// <param name="type">A BlueChilli.Lib.Configuration.DirectoryType enum that represents directory type.</param>
-        /// <returns>The directory path from configuration file.</returns>
-        internal string GetDirectory(DirectoryType type)
-        {
-            switch (type)
-            {
-                case DirectoryType.Images: return ImagesDirectory;
-                case DirectoryType.Scripts: return ScriptsDirectory;
-                case DirectoryType.Styles: return StylesDirectory;
-            }
-
-            throw new ApplicationException($"DirectoryType '{type.ToString()}' not supported.");
-        }
     }
 }
