621f7a9fe119e778ce1c22aa6cfbda4a5b23d854
david <drgrieve@hotmail.com>
2018-03-20 15:08:20 +1100

    add back in google apis script helper

--- src/ChilliSource.Cloud.Web.MVC/Extensions/Helpers/Content/Script.cs
+++ src/ChilliSource.Cloud.Web.MVC/Extensions/Helpers/Content/Script.cs
@@ -6,25 +6,22 @@ namespace ChilliSource.Cloud.Web.MVC
 {
     public static partial class HtmlHelperExtensions
     {
-        ///// <summary>
-        ///// Returns HTML string for the script element of Google map API with key and library parameters from ChilliSource web project configuration file.
-        ///// </summary>
-        ///// <param name="htmlHelper">The System.Web.Mvc.HtmlHelper instance that this method extends.</param>
-        ///// <returns>An HTML string for the script element of Google map API.</returns>
-        //public static MvcHtmlString ScriptGoogleMapApi(this HtmlHelper htmlHelper)
-        //{
-        //    var config = ProjectConfigurationSection.GetConfig();
-        //    var key = config.GoogleApis.ApiKey(config.ProjectEnvironment);
+        /// <summary>
+        /// Returns HTML string for the script element of Google map API with key and library parameters from ChilliSource web project configuration file.
+        /// </summary>
+        /// <param name="htmlHelper">The System.Web.Mvc.HtmlHelper instance that this method extends.</param>
+        /// <returns>An HTML string for the script element of Google map API.</returns>
+        public static MvcHtmlString ScriptGoogleMapApi(this HtmlHelper htmlHelper)
+        {
+            var libraries = GlobalMVCConfiguration.Instance.GoogleApis?.Libraries;
+            var librariesParam = "";
 
-        //    var libraries = config.GoogleApis.Libraries;
-        //    var librariesParam = "";
+            if (!String.IsNullOrWhiteSpace(GlobalMVCConfiguration.Instance.GoogleApis?.Libraries))
+            {
+                librariesParam = String.Format("libraries={0}&", libraries);
+            }
 
-        //    if (!String.IsNullOrWhiteSpace(libraries))
-        //    {
-        //        librariesParam = String.Format("libraries={0}&", libraries);
-        //    }
-
-        //    return MvcHtmlString.Create($@"<script type=""text/javascript"" src=""//maps.googleapis.com/maps/api/js?{librariesParam}key={key}&sensor=false&language=en""></script>");
-        //}
+            return MvcHtmlString.Create($@"<script type=""text/javascript"" src=""//maps.googleapis.com/maps/api/js?{librariesParam}key={GlobalMVCConfiguration.Instance.GoogleApis?.ApiKey}&sensor=false&language=en""></script>");
+        }
     }
 }
