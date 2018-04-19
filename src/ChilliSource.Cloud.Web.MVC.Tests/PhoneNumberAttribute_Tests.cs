ebe2da37e5d512e94a7744436999a1161d26eab2
Will <will@bluechilli.com>
2018-04-19 17:30:33 +1000

    phone number attribute fix

---
+++ src/ChilliSource.Cloud.Web.MVC.Tests/PhoneNumberAttribute_Tests.cs
@@ -0,0 +1,22 @@
+﻿using ChilliSource.Cloud.Core;
+using Moq;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Hosting;
+using Xunit;
+
+namespace ChilliSource.Cloud.Web.MVC.Tests
+{
+    public class PhoneNumberAttribute_Tests
+    {
+        [Fact]
+        public void SimpleTest()
+        {
+            var att = new PhoneNumberAttribute("AU");
+
+            Assert.True(att.IsValid("0411111111"));
+        }
+    }
+}
