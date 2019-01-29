f135027cc98d7fd88188e4f142626c45795939a0
david <drgrieve@hotmail.com>
2019-01-30 10:42:02 +1100

    small enchancement

--- src/ChilliSource.Cloud.Web.MVC/Attributes/EmptyItem.cs
+++ src/ChilliSource.Cloud.Web.MVC/Attributes/EmptyItem.cs
@@ -17,6 +17,11 @@ namespace ChilliSource.Cloud.Web.MVC
         /// </summary>
         public string Text { get; set; }
 
+        /// <summary>
+        /// Do not add an empty item if there is only one item in the list
+        /// </summary>
+        public bool SkipIfSingleItem { get; set; }
+
         public EmptyItemAttribute(string text)
         {
             Text = text;
@@ -25,19 +30,24 @@ namespace ChilliSource.Cloud.Web.MVC
         public void OnMetadataCreated(ModelMetadata metadata)
         {
             metadata.AdditionalValues["EmptyItem-Text"] = Text;
+            if (SkipIfSingleItem)
+            {
+                metadata.AdditionalValues["EmptyItem-SkipSingle"] = true;
+            }
         }
 
         public static IList<SelectListItem> Resolve(ModelMetadata metadata, IEnumerable<SelectListItem> items, IEnumerable<SelectListItem> singleEmptyItem)
         {
             if (items == null) items = ArrayExtensions.EmptyArray<SelectListItem>();
 
+            if (metadata.AdditionalValues.ContainsKey("EmptyItem-SkipSingle") && items.Count() == 1) return items.ToList();
+
             if (metadata.AdditionalValues.ContainsKey("EmptyItem-Text"))
             {
                 var emptyItem = new[] { new SelectListItem { Text = metadata.AdditionalValues["EmptyItem-Text"].ToString(), Value = "" } };
                 return emptyItem.Concat(items).ToList();
             }
- 
-            if (metadata.IsNullableValueType)
+            else if (metadata.IsNullableValueType)
             {
                 if (singleEmptyItem == null)
                     singleEmptyItem = Enumerable.Empty<SelectListItem>();
