bf8f5aa96e501059f71995f4a0a2d65be4609520
drgrieve543 <drgrieve543@gmail.com>
2021-11-23 14:21:18 +1100

    getcurrentarea helper

---
+++ src/ChilliSource.Cloud.Web.MVC/Navigation/NavigationHelpers.cs
@@ -0,0 +1,18 @@
+﻿#if NETCOREAPP
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChilliSource.Cloud.Web.MVC
+{
+    public static class NavigationHelpers
+    {
+        public static string GetCurrentArea(this IHtmlHelper html)
+        {
+            return html.ViewContext.RouteData.Values.GetValueOrDefault("area") as string;
+        }
+    }
+}
+#endif
