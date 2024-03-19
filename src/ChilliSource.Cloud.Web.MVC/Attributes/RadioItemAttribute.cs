e84bc66d48a2b62544f3f2ca9fd941f962dab6cd
drgrieve543 <drgrieve543@gmail.com>
2024-03-19 17:47:22 +1100

    radioitem attribute

---
+++ src/ChilliSource.Cloud.Web.MVC/Attributes/RadioItemAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using System;
+
+namespace ChilliSource.Cloud.Web.MVC
+{
+    public class RadioItemAttribute : Attribute, IMetadataAware
+    {
+
+        public RadioItemAttribute()
+        {
+        }
+
+        public void GetDisplayMetadata(DisplayMetadataProviderContext metadata)
+        {
+            metadata.DisplayMetadata.AdditionalValues["RadioItem"] = true;
+        }
+    }
+}
